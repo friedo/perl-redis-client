f5461e62f812a0ed3cdb0db81f0450b534eb50ea
Mike Friedman <friedo@friedo.com>
2011-10-02 18:18:59 -0400

    bulk reply parser and echo cmd

--- lib/Redis/Client.pm
+++ lib/Redis/Client.pm
@@ -2,11 +2,13 @@ package Redis::Client;
 
 use Moose;
 use IO::Socket::INET;
+use Carp 'croak';
 
 has 'host'         => ( is => 'ro', isa => 'Str', default => 'localhost' );
 has 'port'         => ( is => 'ro', isa => 'Int', default => 6379 );
 has '_sock'        => ( is => 'ro', isa => 'IO::Socket', init_arg => undef, lazy_build => 1 );
 
+my $CRLF = "\x0D\x0A";
 
 sub _build__sock { 
     my $self = shift;
@@ -18,6 +20,68 @@ sub _build__sock {
     ) or die sprintf q{Can't connect to Redis host at %s:%s: %s}, $self->host, $self->port, $@;
 
     return $sock;
+}
+
+
+# ECHO command
+sub echo { 
+    my $self = shift;
+    my $arg  = shift;
+
+    croak 'No string provided for Redis ECHO command'
+      unless defined $arg;
+
+    my $res = $self->_send_command( 'ECHO', $arg );
+}
+
+sub _send_command { 
+    my $self = shift;
+    my ( $cmd, @args ) = @_;
+
+    my $sock = $self->_sock;
+    my $cmdline = sprintf '%s %s%s', $cmd, ( join ' ' => @args ), $CRLF;
+
+    $sock->send( $cmdline );
+
+    return $self->_get_response;
+}
+
+sub _get_response { 
+    my $self = shift;
+    my $sock = $self->_sock;
+
+    # the first byte tells us what to expect
+    my %msg_types = ( '+'   => '_read_single_line',
+                      '-'   => '_read_single_line',
+                      ':'   => '_read_integer',
+                      '$'   => '_read_bulk_reply',
+                      '*'   => '_read_multi_bulk_reply' );
+
+    my $buf;
+    $sock->recv( $buf, 1 );
+    die "Can't read from socket" unless $buf;
+    die "Can't understand Redis message type [$buf]" unless exists $msg_types{$buf};
+
+    my $meth = $msg_types{$buf};
+
+    return $self->$meth;
+}
+
+sub _read_bulk_reply { 
+    my $self = shift;
+    my $sock = $self->_sock;
+
+    local $/ = $CRLF;
+
+    my $length = readline $sock;
+    chomp $length;
+    my $buf;
+    $sock->read( $buf, $length );
+
+    # throw out the terminating CRLF
+    readline $sock;
+
+    return $buf;
 }
 
 1;
--- t/conn.t
+++ t/conn.t
@@ -3,7 +3,16 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 4;
 
 use_ok 'Redis::Client';
+
+my $redis = Redis::Client->new;
+
+ok $redis;
+isa_ok $redis, 'Redis::Client';
+
+my $result = $redis->echo( 'foobar' );
+
+is $result, 'foobar';
 

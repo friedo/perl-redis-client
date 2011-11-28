396294820dfb65eda57acdc7fe3e261f92b21819
Rebecca Turner <me+github@winterseale.com>
2011-11-28 12:36:05 -0500

    Fix bugs in determining local Redis server-- defaults didn't work

--- t/RedisClientTest.pm
+++ t/RedisClientTest.pm
@@ -7,12 +7,12 @@ use Redis::Client;
 
 sub server { 
     my $host = $ENV{PERL_REDIS_TEST_SERVER}   || 'localhost';
-    my $port = $ENV{PERL_REDIS_TEST_SERVER}   || '6379';
+    my $port = $ENV{PERL_REDIS_TEST_PORT}     || '6379';
     my $pw   = $ENV{PERL_REDIS_TEST_PASSWORD} || undef;
 
     my $client = eval { 
-        Redis::Client->new( host => $ENV{PERL_REDIS_TEST_SERVER},
-                            port => $ENV{PERL_REDIS_TEST_PORT},
+        Redis::Client->new( host => $host,
+                            port => $port,
                             $pw ? ( password => $pw ) : ( ) );
     };
 
